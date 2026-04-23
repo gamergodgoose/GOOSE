134cab63bb2ab2ef1994b6413a6ce3248d664190
JyoshnaMP <jyoshna.d@moonpreneur.com>
2026-04-23 08:14:18 +0530

    Update roomRoutes.js

--- Routes/roomRoutes.js
+++ Routes/roomRoutes.js
@@ -1,6 +1,8 @@
-const express =require("express");
-const router=express.Router();
-const {createRoom,joinRoom}=require('../Controllers/Room');
-router.post("/create",createRoom);
-router.post("/join",joinRoom);
-module.exports=router;
+const express = require("express");
+const router = express.Router();
+const { createRoom, joinRoom } = require('../Controllers/Room');
+
+router.post("/create", createRoom);
+router.post("/join", joinRoom);
+
+module.exports = router;
